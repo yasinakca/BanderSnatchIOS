83595f484656c8af51e245ccfecca97615ddf12b
yasinakca <yasinakcadev@gmail.com>
2020-10-18 15:06:26 +0300

    final

--- BanderSnatchIOS/Controller/ViewController.swift
+++ BanderSnatchIOS/Controller/ViewController.swift
@@ -12,12 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var firstChoice: UIButton!
     @IBOutlet weak var secondChoice: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
     @IBAction func choiseMade(_ sender: UIButton) {
+        storyBrain.checkChoice(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryText()
+        firstChoice.setTitle(storyBrain.getChoice1(), for: .normal)
+        secondChoice.setTitle(storyBrain.getChoice2(), for: .normal)
     }
     
 }
